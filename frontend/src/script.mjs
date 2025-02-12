2cd77699173fbe26cd255a4ad12df23001e312fe
ozysouza <ozysouzaa@gmail.com>
2025-02-12 00:55:42 -0500

    Add JQuery script to return movies from API and search for new ones. Create div with function

---
+++ frontend/src/script.mjs
@@ -0,0 +1,49 @@
+const APILINK = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${API_KEY}&page=1`;
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
+const SEARCHAPI = `https://api.themoviedb.org/3/search/movie?&api_key=${API_KEY}&query=`;
+
+const main = $("#section");
+const form = $("#form");
+const search = $("#query");
+
+function returnMovies(url) {
+    fetch(url)
+        .then(res => res.json())
+        .then(function (data) {
+            main.empty(); // Clear existing content
+
+            data.results.forEach(element => {
+                const div_row = $('<div>').addClass('row');
+                const div_column = $('<div>').addClass('column');
+                const div_card = $('<div>').addClass('card');
+
+                const image = $('<img>')
+                    .addClass('thumbnail')
+                    .attr('id', 'card-img')
+                    .attr('src', IMG_PATH + element.poster_path);
+
+                const title = $('<h3>')
+                    .attr('id', 'card-title')
+                    .text(element.title);
+
+                main.append(div_row);
+                div_row.append(div_column);
+                div_column.append(div_card);   
+                div_card.append(image, title);
+            });
+        });
+}
+
+// Initial call to load movies
+returnMovies(APILINK);
+
+form.on("submit", (event) => {
+    event.preventDefault();
+    main.empty(); // Clear existing content
+
+    const searchItem = search.val();
+    if (searchItem) {
+        returnMovies(SEARCHAPI + searchItem);
+        search.val('');
+    }
+});
