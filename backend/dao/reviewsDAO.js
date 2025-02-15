7f1fdad2c196bf016b8d8da286a7be445c0ce073
ozysouza <ozysouzaa@gmail.com>
2025-02-15 01:55:13 -0500

    Create addReview func

--- backend/dao/reviewsDAO.js
+++ backend/dao/reviewsDAO.js
@@ -5,7 +5,7 @@ const ObjectId = mongodb.ObjectId;
 let reviews;
 
 export default class ReviewsDAO {
-    static async injectDB(conn) {
+      async injectDB(conn) {
         if (reviews) {
             return
         }
@@ -14,6 +14,20 @@ export default class ReviewsDAO {
             reviews = await conn.db("reviews").collection("reviews");
         } catch (error) {
             console.error(`Enable to establish collection handles in userDAO: ${error}`)
+        }
+    }
+
+    static async addReview(movieId, user, review) {
+        try {
+            const reviewDoc = {
+                movieId: movieId,
+                user: user,
+                review: review
+            }
+
+            return await reviews.insertOne(reviewDoc);
+        } catch (error) {
+            console.error(`Enable to post review: ${error}`)
         }
     }
 }
